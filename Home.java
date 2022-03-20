12e0cda59ad4b7c450e46e297027a5a6abec1f15
sayandey <sayandeypersonal@gmail.com>
2022-03-20 17:36:38 +0530

    Manas ei malta

--- Home.java
+++ Home.java
@@ -1,29 +1,16 @@
+
 import javax.swing.*;
 import java.util.*;
 import java.io.*;
 import java.awt.event.*;
 import javax.swing.ImageIcon;
 
-class Home implements ActionListener{
+class HomeClass{
     
     JFileChooser filechooser;
-    public String imagePath;
+    JLabel label = new JLabel();
 
-    //overriding actionperformed
-    @Override
-    public void actionPerformed(ActionEvent e){
-        int selected = filechooser.showDialog(null,"Open file");
-        if(selected == JFileChooser.APPROVE_OPTION){
-            System.out.println("Selected file path: " + filechooser.getSelectedFile().getAbsolutePath());
-            String path = filechooser.getSelectedFile().getAbsolutePath();
-            imagePath = path;
-        }
-        else{
-            System.out.println("Cancelled");
-        }
-    }
-
-    public Home(){
+    public HomeClass(){
         JFrame home = new JFrame("College project AC Sir");
         home.setSize(500,500);
         home.setVisible(true);
@@ -32,34 +19,45 @@ class Home implements ActionListener{
         JPanel panel = new JPanel();
         home.add(panel);
         JButton browse = new JButton("Browse");
-        JButton save = new JButton("Save");
+        JButton save = new JButton("Dummy");
         panel.add(browse);
         panel.add(save);
-
-        //creating jlabel
-        // JLabel label = new JLabel();
-        // label.setIcon(new ImageIcon(imagePath));
-        // panel.add(label);
-        System.out.println(imagePath);
-        //creating textpane
-
 
         //creating filechooser object
         filechooser = new JFileChooser();
 
         //adding eventlistner
-        browse.addActionListener(this);
+        //browse.addActionListener(this);
+        browse.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                int selected = filechooser.showDialog(null,"Open file");
+                if(selected == JFileChooser.APPROVE_OPTION){
+                    System.out.println("Selected file path: " + filechooser.getSelectedFile().getAbsolutePath());
+                    String path = filechooser.getSelectedFile().getAbsolutePath();
+                    label.setIcon(new ImageIcon(path));
+                    label.setSize(300,300);
+                }
+                else{
+                    System.out.println("Cancelled");
+                }
+            }
+        });
+        //adding the label that contains the pic
+        panel.add(label);
     }
+}
 
+public class Home{
     public static void main(String args[]) throws IOException
     {
         SwingUtilities.invokeLater(
             new Runnable(){
                 public void run()
                 {
-                    new Home();  //running the java file
+                    HomeClass obj = new HomeClass();  //running the java file
                 }
             }
         );
     }
 }
+
